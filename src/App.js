aba9ef527ef33fb80724a74d1bcb435603a11f90
webhero-nur <nurealom522@gmail.com>
2023-02-05 00:31:36 +0600

    What is React, when to use it, react vs angular vs vue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Device from './components/Device/Device';
+import Tablet from './components/Tablet/Tablet';
 import Watch from './components/Watch/Watch';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Device name="uPhone" price="15000"></Device>
       <Watch></Watch>
+      <Tablet></Tablet>
     </div>
   );
 }
