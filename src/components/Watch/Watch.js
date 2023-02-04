742b9c244ca400932d1ec67d6e505927b96e538e
webhero-nur <nurealom522@gmail.com>
2023-02-04 23:55:46 +0600

    How React Hook works, send state via props, props vs state

--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Display from '../Display/Display';
 
 const Watch = () => {
     const [steps, setSteps] = useState(0);
@@ -13,10 +14,11 @@ const Watch = () => {
     }, [steps])
 
     return (
-        <div>
+        <div style={{ border: '2px solid purple', margin: '20px' }}>
             <h2>This is my smart watch</h2>
             <h3>My Current Steps: {steps}</h3>
             <button onClick={increaseSteps}>De Dour.............</button>
+            <Display name="garmin" steps={steps}></Display>
         </div>
     );
 };
